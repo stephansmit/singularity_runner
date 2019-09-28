2df74774e533f7f7ed94c150ce06482a66d50c43
anonymous <s.h.h.j.smit@tudelft.nl>
2019-09-28 19:04:12 +0200

    added the runner

--- sinexecuter.py
+++ sinexecuter.py
@@ -1,26 +1,26 @@
 from spython.main import Client as client
-
+import subprocess
+import os
 class SinExecuter(object):
-    def __init__(self, name, url, img_dir):
+    def __init__(self, img_name, img_url, img_dir):
         self.img_name = img_name
         self.img_url = img_url
         self.img_dir = img_dir
 
     def _make_dir(self):
-        subprocess.check_call(['mkdir', '-p', img_dir])
+        subprocess.check_call(['mkdir', '-p', self.img_dir])
 
     def _pull_image(self):
-        client.pull(image=self.image_url,name=self.image_name, pull_folder=self.image_dir)
+        client.pull(image=self.img_url,name=self.img_name, pull_folder=self.img_dir)
         return
 
     def run(self, cmd, bind_dir):
         self._make_dir()
         self._pull_image()
-        client.load(os.path.join(self.image_dir,self.image_name))
+        client.load(os.path.join(self.img_dir,self.img_name))
         output = client.execute(cmd,
-                                bind=','.join(bind_dir+":/data"),
+                                bind=bind_dir+":/data",
                                 options=['--pwd','/data', '--cleanenv'], stream=True)
-        with open(log.fname, 'w', buffering=100) as lfile:
-            for line in output:
-                lfile.write(line)
+        for line in output:
+            print(line)
 
