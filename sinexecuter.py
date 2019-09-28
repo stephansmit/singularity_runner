90253b993e09cfe01cf1a5a1e0e87dad868f5f6c
anonymous <s.h.h.j.smit@tudelft.nl>
2019-09-28 18:48:05 +0200

    fixed

--- sinexecuter.py
+++ sinexecuter.py
@@ -1,14 +1,13 @@
 from spython.main import Client as client
 
-class SinExecuter(object)
+class SinExecuter(object):
     def __init__(self, name, url, img_dir):
         self.img_name = img_name
         self.img_url = img_url
         self.img_dir = img_dir
 
-
     def _make_dir(self):
-        subprocess.check_call(['mkdir', '-p', img_dir)
+        subprocess.check_call(['mkdir', '-p', img_dir])
 
     def _pull_image(self):
         client.pull(image=self.image_url,name=self.image_name, pull_folder=self.image_dir)
@@ -19,7 +18,7 @@ class SinExecuter(object)
         self._pull_image()
         client.load(os.path.join(self.image_dir,self.image_name))
         output = client.execute(cmd,
-                                bind=','.join(bind_dir+":/data"]),
+                                bind=','.join(bind_dir+":/data"),
                                 options=['--pwd','/data', '--cleanenv'], stream=True)
         with open(log.fname, 'w', buffering=100) as lfile:
             for line in output:
